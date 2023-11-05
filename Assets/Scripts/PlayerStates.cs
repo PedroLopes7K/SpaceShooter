4fd1e426346bb4d3e5e282a40a835e8ef0a48f6b
PedroLopes7K <pedrohenri3601@gmail.com>
2023-11-04 22:09:12 -0300

    manage health bar image

--- Assets/Scripts/PlayerStates.cs
+++ Assets/Scripts/PlayerStates.cs
@@ -1,16 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerStates : MonoBehaviour
 {
 
     private float maxHealth = 3;
     private float health;
+    [SerializeField] private Image healthFill;
     // Start is called before the first frame update
     void Start()
     {
         health = maxHealth;
+        healthFill.fillAmount = health / maxHealth;
     }
 
     // Update is called once per frame
@@ -23,6 +26,7 @@ public class PlayerStates : MonoBehaviour
     public void PlayerTakeDamage(float damage)
     {
         health -= damage;
+        healthFill.fillAmount = health / maxHealth;
 
         if (health <= 0)
         {
